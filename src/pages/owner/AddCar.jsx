7e0da97250e4611a35ca6b1d1c18ea19b6b228c7
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-18 09:49:09 -0300

    car brand and model inputs added to add car page

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -33,6 +33,18 @@ const AddCar = () => {
           </label>
           <p className=' text-sm text-gray-500'>Upload picture of your car</p>
         </div>
+
+        <div className=' grid grid-cols-1 md:grid-cols-2 gap-6'>
+          <div className=' flex flex-col w-full'>
+            <label >Brand</label>
+            <input type="text" placeholder=' eg. BMW, Mercedes, Audi...' required className=' px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.brand} onChange={e=>setCar({...car, brand: e.target.value})} />
+          </div>
+          
+          <div className=' flex flex-col w-full'>
+            <label >Model</label>
+            <input type="text" placeholder=' eg. BMW, Mercedes, Audi...' required className=' px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.model} onChange={e=>setCar({...car, model: e.target.value})} />
+          </div>
+        </div>
       </form>
     </div>
   )
