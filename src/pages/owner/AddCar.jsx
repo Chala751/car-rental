e38479da3838228ba1b2ed3dc70dfa8caceb145b
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-18 10:16:55 -0300

    year price and catagory selection added to add car page

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -4,6 +4,8 @@ import { assets } from '../../assets/assets'
 
 const AddCar = () => {
 
+  const currency=import.meta.env.VITE_CURRENCY
+
   const [image,setImage]= useState(null)
   const[car,setCar]=useState({
     brand: '',
@@ -42,9 +44,32 @@ const AddCar = () => {
           
           <div className=' flex flex-col w-full'>
             <label >Model</label>
-            <input type="text" placeholder=' eg. BMW, Mercedes, Audi...' required className=' px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.model} onChange={e=>setCar({...car, model: e.target.value})} />
+            <input type="text" placeholder=' eg. X5, E-Class, M4...' required className=' px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.model} onChange={e=>setCar({...car, model: e.target.value})} />
           </div>
         </div>
+
+        <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          <div className=' flex flex-col w-full'>
+            <label >Year</label>
+            <input type="number" placeholder='2025' required className=' px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.year} onChange={e=>setCar({...car, year: e.target.value})} />
+          </div>
+
+          <div className=' flex flex-col w-full'>
+            <label >Daily Price ({currency})</label>
+            <input type="text" placeholder='100 ' required className=' px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.pricePerDay} onChange={e=>setCar({...car, pricePerDay: e.target.value})} />
+          </div>
+
+          <div className=' flex flex-col w-full'>
+            <label >Category</label>
+            <select value={car.category} onChange={e=>setCar({...car, category: e.target.value})} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+              <option value="">Select Category</option>
+              <option value="Sedan">Sedan</option>
+              <option value="SUV">SUV</option>
+              <option value="Van">Sedan</option>
+            </select>
+          </div>
+        </div>
+        
       </form>
     </div>
   )
