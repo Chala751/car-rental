6c8256c77c3a637855a985bdd9ac3b3b82069b64
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-19 04:00:38 -0300

    title added to the manage cars page

--- src/pages/owner/ManageCar.jsx
+++ src/pages/owner/ManageCar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCarData } from '../../assets/assets'
+import Title from '../../components/owner/Title'
 
 const ManageCar = () => {
+
+  const[car,setCar]=useState([])
+  const fetchOwnerCars=async()=>{
+    setCar(dummyCarData)
+  }
+  useEffect(()=>{
+    fetchOwnerCars()
+  },[])
   return (
-    <div>
+    <div className=' px-4 py-10 md:px-10 w-full'>
+      <Title title="Manage Cars" subTitle="View all listed cars, update their details, or remove them from the booking platform."/>
       
     </div>
   )
