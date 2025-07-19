e034e0fb986e600f57b996a2cc38294c8be75bfd
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-19 08:04:02 -0300

    Fix: remove inner git repo from client and track as normal folder

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
+import Testimonal from '../components/Testimonal'
+import Newsletter from '../components/Newsletter'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeaturedSection/>
+      <Banner/>
+      <Testimonal/>
+      <Newsletter/>
+    </>
+  )
+}
+
+export default Home
