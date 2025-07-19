032b7141621d07b4e74de8c7d283bee3c86cadd3
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-19 04:50:03 -0300

    title added to manage bookings page

--- src/pages/owner/ManageBookings.jsx
+++ src/pages/owner/ManageBookings.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { assets, dummyMyBookingsData,  } from '../../assets/assets'
+import Title from '../../components/owner/Title'
 
 const ManageBookings = () => {
+   const[bookings,setBookings]=useState([])
+    const fetchOwnerBookings=async()=>{
+      setCars(dummyMyBookingsData)
+    }
+    useEffect(()=>{
+      fetchOwnerBookings
+    },[])
   return (
-    <div>
+    <div className=' px-4 py-10 md:px-10 w-full'>
+      <Title title="Manage Bookings" subTitle="Track all customer bookings, approve or cancle requests, and manage booking status."/>
       
     </div>
   )
