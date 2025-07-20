ce3654c6da6d787e39738f2f623391f5a07f4d0c
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-20 03:37:57 -0300

    register user in the usercontroller

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,34 @@
+import User from '../models/User.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+//generate jwt token
+const generateToken = (userId)=>{
+    const payload =userId;
+    return jwt.sign(payload, process.env.JWT_SECRET)
+}
+
+//register user
+export const registerUser = async (req,res)=>{
+    try {
+        const {name, email, password}= req.body
+
+        if (!name || !email || !password || password.length<8) {
+            return res.json({success: false, message: 'Fill all fields'})
+        }
+
+        const userExists= await User.findOne({email})
+
+        if (userExists) {
+            return res.json({success: false, message: 'User already exist'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10)
+        const user =await User.create({name,email,password:hashedPassword})
+        const token= generateToken(user._id.toString())
+        res.json({success: true, token})
+    } catch (error) {
+       console.log(error.message) ;
+       res.json({success: false, message:error.message})
+    }
+}
