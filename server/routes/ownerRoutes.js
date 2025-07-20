b3368c1b8ea4e350999023e1c1fc17c3f3e0a0f2
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-20 10:30:42 -0300

    multer added as a middleware

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { protect } from '../middleware/auth';
+import { protect } from '../middleware/auth.js';
 import { changeRoleToOwner } from '../controllers/ownerController.js';
 
 const ownerRouter =express.Router();
