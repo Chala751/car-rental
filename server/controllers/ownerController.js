bdee9213c98f0775ba8d9d5a27e571a3f3f2ffab
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-20 11:33:26 -0300

    upload imagekit and optimize in owner controller then routed

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import Car from '../models/Car.js'
+import fs from 'fs'
 
 
 export const changeRoleToOwner =async(req,res)=>{
@@ -18,7 +20,29 @@ export const addCar=async(req,res)=>{
         const {_id}=req.user;
         let car= JSON.parse(req.body.carData);
         const imageFile=req.file;
-        
+
+        //upload image to imagekit
+        const fileBuffer=fs.readFileSync(imageFile.path)
+        const response=await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        })
+
+        // optimize through imagekit  URL transformation
+        var optimizedImageUrl = imagekit.url({
+          path : response.filePath,
+          transformation : [
+            { width : "1280" },
+            {quality :"auto"},
+            {format :"webp"}
+            ]
+        });
+
+        const image=optimizedImageUrl;
+        await Car.create({...car, owner:_id, image})
+        res.json({success: true, message: "Car Added"})
+
     } catch (error) {
         console.log(error.message)
         res.json({success: flase, message: error.message})
