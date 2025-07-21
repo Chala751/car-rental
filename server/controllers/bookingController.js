43a7718ce762b15c4cbfcf4b4e1aaefa69b2b39f
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-21 06:20:44 -0300

    api to create booking added to booking controller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -32,3 +32,30 @@ export const checkAvailabilityOfCar=async(res,req)=>{
         res.json({success: flase, message: error.message})
     }
 }
+
+//api to create booking
+export const createBooking=async(res,req)=>{
+    try {
+        const {_id}=req.user;
+        const {location, pickupDate,returnDate}=req.body;
+
+        const isAvaliable=await checkAvailability(car, pickupDate, returnDate)
+        if (!isAvaliable) {
+            res.json({success: false, message: "Car ia not available"})
+        }
+
+        const carData = await Car.findById(car)
+
+        const picked = new Date(pickupDate);
+        const returned = new Date(returnDate);
+        const noOfDays = Math.ceil((returned-picked)/(1000*60*60*24));
+        const price = carData.pricePerDay*noOfDays;
+
+        await Booking.create({car, owner: carData.owner, user:_id, pickupDate, returnDate, price})
+        res.json({success: true, message: "Booking Created"})
+       
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: flase, message: error.message})
+    }
+}
