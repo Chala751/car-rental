db2691df6390bbb62cdadb6b5eb96906af70e83f
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-21 05:50:36 -0300

    api to check availability of cars for given date and location added to bookingcontroller

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,5 @@
 import Booking from "../models/Booking.js"
+import Car from '../models/Car.js'
 
 
 const checkAvailability = async(car, pickupDate,returnDate)=>{
@@ -8,4 +9,26 @@ const checkAvailability = async(car, pickupDate,returnDate)=>{
         returnDate: {$gte: pickupDate},
     })
     return bookings.length === 0;
+}
+
+//api to check availability of cars for given date and location
+export const checkAvailabilityOfCar=async(res,req)=>{
+    try {
+        const {location, pickupDate,returnDate}=req.body;
+
+        const cars=await Car.find({location, isAvaliable: true})
+
+        const availabileCarsPromises = cars.map(async(car)=>{
+            const isAvaliable = await checkAvailability(car._id, pickupDate, returnDate)
+            return {...car._doc, isAvaliable: isAvaliable}
+        })
+
+        let availabileCars=await Promise.all(availabileCarsPromises);
+        availabileCars=availabileCars.filter(car=> car.isAvaliable === true)
+
+        res.json({success: true, availabileCars})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: flase, message: error.message})
+    }
 }
