58f8505041f22b9dafe7e968078554be82fb6610
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-22 13:36:35 -0300

    api end point routed to the update api

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protect } from '../middleware/auth.js';
-import { addCar, changeRoleToOwner, deleteCar, getDashboardData, getOwnerCars, toggleCarAvailability } from '../controllers/ownerController.js';
+import { addCar, changeRoleToOwner, deleteCar, getDashboardData, getOwnerCars, toggleCarAvailability, updateUserImage } from '../controllers/ownerController.js';
 import upload from '../middleware/multer.js'
 
 const ownerRouter =express.Router();
@@ -11,5 +11,6 @@ ownerRouter.get("/cars",protect, getOwnerCars)
 ownerRouter.post("/toggle-car",protect, toggleCarAvailability)
 ownerRouter.post("/delete-car",protect, deleteCar)
 ownerRouter.get("/dashboard",protect, getDashboardData)
+ownerRouter.get("/update-image",upload.single("image"),protect, updateUserImage)
 
 export default ownerRouter
