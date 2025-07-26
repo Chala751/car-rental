72dcf6118cca5a5c6bae504b264e4d7ab4ca9f9b
Chala Temesgen <ctemesgen85@gmail.com>
2025-07-26 11:43:53 -0300

    searshing in hero section conncetd to its api

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import {assets, cityList} from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 const Hero = () => {
 
     const [pickupLocation ,setPickupLocation]=useState('')
+    const {pickupDate,setPickupDate,returnDate,setReturnDate,navigate}=useAppContext()
 
-     
+    const handleSearch=(e)=>{
+      e.preventDefault()
+      navigate('/cars? pickupLocation='+ pickupLocation+ '&pickupDate='+pickupDate+ '&returnDate='+returnDate)
+    }
   return (
     <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
       <h1 className='text-4xl md:text-4xl font-semibold'>Luxery cars on Rent</h1>
 
-      <form className='felx flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white' >
+      <form onSubmit={handleSearch} className='felx flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white' >
         <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-18'>
             <div className='flex flex-col items-start gap-3'>
                 <select required value={pickupLocation} onChange={(e)=>setPickupLocation(e.target.value)}>
@@ -21,11 +26,11 @@ const Hero = () => {
             </div>
             <div className='flex flex-col items-start gap-3'>
                 <label htmlFor="pickup-date">Pick-up Date</label>
-                <input type="date" id='pickup-date' min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500'  required/>
+                <input value={pickupDate} onChange={e=>setPickupDate(e.target.value)} type="date" id='pickup-date' min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500'  required/>
             </div>
             <div className='flex flex-col items-start gap-3'>
                 <label htmlFor="return-date">Return Date</label>
-                <input type="date" id='return-date'  className='text-sm text-gray-500'  required/>
+                <input value={returnDate} onChange={e=>setReturnDate(e.target.value)} type="date" id='return-date'  className='text-sm text-gray-500'  required/>
             </div>
             <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primery hover:bg-primery-dull text-white rounded-full cursor-pointer'>
                 <img src={assets.search_icon} alt="search" className='brightness-300' />
